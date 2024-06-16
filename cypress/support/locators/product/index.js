e21f6aa3e0eba19fb06e01a7b02cb2d8bc4926ca
adtet <adiyatma.padang@gmail.com>
2024-06-16 14:53:39 +0700

    adit commit repair add product test

--- cypress/support/locators/product/index.js
+++ cypress/support/locators/product/index.js
@@ -1,16 +1,15 @@
 module.exports = {
   button: {
     tambah: '//a[@href="/products/create"]',
-    simpan:'//button[contains(text(), "simpan")]'
+    simpan: ".chakra-button.css-l5lnz6",
   },
-  input:{
-    kode:'//*[@id="kode"]',
-    nama:'//*[@id="nama"]',
-    deskripsi:'//*[@id="deskripsi"]',
-    harga_beli:'//*[@id="harga beli"]',
+  input: {
+    kode: '//*[@id="kode"]',
+    nama: '//*[@id="nama"]',
+    deskripsi: '//*[@id="deskripsi"]',
+    harga_beli: '//*[@id="harga beli"]',
     harga_jual: '//*[@id="harga jual"]',
-    stok:'//*[@id="stok"]',
-    kategori:'//*[@id="kategori"]',
-
-  }
+    stok: '//*[@id="stok"]',
+    kategori: '//*[@id="kategori"]',
+  },
 };
--- cypress/support/pages/product/index.js
+++ cypress/support/pages/product/index.js
@@ -1,6 +1,6 @@
 const locatorHome = require("../../locators/home");
 const locatorProduct = require("../../locators/product");
-const data= require("../../data-test/product")
+const data = require("../../data-test/product");
 
 class Product {
   async openProduct() {
@@ -10,33 +10,41 @@ class Product {
   async openTambahProduct() {
     cy.xpath(locatorProduct.button.tambah).click();
   }
-  async inputKode(){
-cy.xpath(locatorProduct.input.kode).type(data.kode)
+  async inputKode() {
+    cy.xpath(locatorProduct.input.kode).type(data.kode);
   }
-  async inputNama(){
-    cy.xpath(locatorProduct.input.nama).type(data.nama)
+  async inputNama() {
+    cy.xpath(locatorProduct.input.nama).type(data.nama);
   }
-  async inputDeskripsi(){
-    cy.xpath(locatorProduct.input.deskripsi).type(data.deskripsi)
+  async inputDeskripsi() {
+    cy.xpath(locatorProduct.input.deskripsi).type(data.deskripsi);
+  }
+  async inputHargaBeli() {
+    cy.xpath(locatorProduct.input.harga_beli).type(data.harga_beli);
+  }
+  async inputHargaJual() {
+    cy.xpath(locatorProduct.input.harga_jual).type(data.harga_jual);
+  }
+  async inputStok() {
+    cy.xpath(locatorProduct.input.stok).type(data.stok);
+  }
+  async inputKategori() {
+    cy.xpath(locatorProduct.input.kategori).click();
 
-  }
-  async inputHargaBeli(){
-    cy.xpath(locatorProduct.input.harga_beli).type(data.harga_beli)
-  }
-async inputHargaJual(){
-  cy.xpath(locatorProduct.input.harga_jual).type(data.harga_jual)
+    // Assert that the modal is visible
+    cy.get(".chakra-modal__content").should("be.visible");
 
-}
-async inputStok(){
-  cy.xpath(locatorProduct.input.stok).type(data.stok)
+    // Type in the search/filter input (if needed)
+    cy.get(".chakra-input.css-2s2hk4").type(data.kategori);
 
-}
-async inputKategori(){
-  cy.xpath(locatorProduct.input.kategori).type(data.kategori)
-}
-async klikSimpanButton(){
-  cy.xpath(locatorProduct.button.simpan).click()
-}
+    // Select the row from the table based on its text content
+    cy.contains(".chakra-table tbody tr", data.kategori).click();
+
+    // Assert that the input field value is set correctly
+  }
+  async klikSimpanButton() {
+    cy.get(locatorProduct.button.simpan).click();
+  }
 }
 
 module.exports = new Product();
