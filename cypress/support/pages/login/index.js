c4c5cf777a5b4c649693e0d12af0b54cdd665e2d
adtet <adiyatma.padang@gmail.com>
2024-06-16 12:49:42 +0700

    adit commit yes oke

--- cypress/support/pages/login/index.js
+++ cypress/support/pages/login/index.js
@@ -4,7 +4,7 @@ const data = require("../../data-test/login")
 
 class LoginPage {
   async visitLoginPageUrl() {
-    cy.visit("https://kasirdemo.belajarqa.com/login");
+    cy.visit("https://kasiraja.ajikamaludin.id");
   }
 
   async verifyLoginPageAppears() {
